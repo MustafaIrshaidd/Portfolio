276ca30344e299d4c33fcfa6b259a3ed6c701658
mustafairshaidd <mustafa-irshaid_2001@hotmail.com>
2023-07-31 18:43:26 +0300

    implemented changing background image transition and dynamic heights for education section

---
+++ assets/js/index.js
@@ -0,0 +1,91 @@
+const mainContainer = document.querySelector("main");
+
+let lastScrollY = mainContainer.scrollTop;
+
+const detectScrollUp = () => {
+  const currentScrollY = mainContainer.scrollTop;
+
+  if (currentScrollY > lastScrollY) {
+    lastScrollY = currentScrollY;
+    return false;
+  }
+
+  lastScrollY = currentScrollY;
+  return true;
+};
+
+let categoryContentIDForOffsetMap = new Map();
+
+const getElementPositions = () => {
+  document.querySelectorAll("header, section").forEach((element, index) => {
+    const rect = element.getBoundingClientRect();
+    const yOffset = rect.top + window.scrollY;
+    const yOffsetFloored = Math.floor(yOffset / 10);
+    categoryContentIDForOffsetMap.set(yOffsetFloored, index + 1);
+  });
+};
+
+window.onload = () => {
+  getElementPositions();
+
+  const education = document.querySelector(".education-intervals");
+
+  const educationFirstElement = document.querySelector(
+    ".education-intervals > li:nth-of-type(1)"
+  );
+
+  const educationLastElement = document.querySelector(
+    ".education-intervals > li:nth-last-of-type(1)"
+  );
+
+  const educationLinkContainer = educationFirstElement.querySelector(
+    ".education-intervals-links-container"
+  );
+
+  // Get the computed styles of the element
+  const educationStyles = window.getComputedStyle(educationLinkContainer);
+
+  // Create a new style rule for the ::after pseudo-element
+  const bgIntervalsHeight = `
+  .education-intervals li:nth-of-type(1) .education-intervals-links::after {
+    height:calc(${education.clientHeight}px + 80vh - ${educationFirstElement.clientHeight}px + 5.1rem);
+  }
+`;
+
+  const lineHeight = `
+    .education-intervals li:nth-last-of-type(1) .education-intervals-links::after {
+    height:calc(${education.clientHeight}px + 20vh - ${educationLastElement.clientHeight}px + 2.5rem);
+  }
+`;
+
+  // Create a new <style> element to hold the style rule
+  const styleElement = document.createElement("style");
+  styleElement.textContent = bgIntervalsHeight + lineHeight;
+
+  // Add the <style> element to the document's <head>
+  document.head.appendChild(styleElement);
+};
+
+window.onresize = () => {
+  location.reload();
+};
+
+function reveal(event) {
+  if (
+    categoryContentIDForOffsetMap.has(Math.floor(mainContainer.scrollTop / 10))
+  ) {
+    const value = categoryContentIDForOffsetMap.get(
+      Math.floor(mainContainer.scrollTop / 10)
+    );
+
+    if (!detectScrollUp()) {
+      for (let i = 1; i < value; i++) {
+        document.querySelector(`.bg-overlay-${i}`).style.opacity = 0;
+      }
+    }
+
+    document.querySelector(`.bg-overlay-${value}`).style.opacity = 1;
+  }
+}
+
+mainContainer.addEventListener("scroll", reveal);
